e869db9c5b53f8a40e67b171f1b7f5e7393c7e23
seunghoonlee <leeseunghoon88@gmail.com>
2015-07-21 17:10:50 -0400

    Mongodb: Insert & Retrieve
    can insert into ‘users’ table
    can retrieve from ‘users’ table

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var monk = require('monk');
 // var db = monk('localhost:27017/CSC309A3');
 var mongoClient = mongodb.MongoClient;
 var mongourl = 'mongodb://localhost:27017/CSC309A3';
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
 /*	
 var routes = require('./routes/index');
 var users = require('.routes/users');
@@ -22,6 +24,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 });
 */
 // https://codeforgeek.com/2014/09/handle-get-post-request-express-4/
+// http://code.tutsplus.com/tutorials/getting-started-with-mongodb-part-1--net-22879
+
+// MongoDb terms	collection = table, document = tuple 
+/* Mongodb commands
+
+show dbs
+show collections
+
+use DATABASE_NAME
+db.dropDatabase()
+db.createCollection(name, options)
+db.COLLECTION_NAME.drop()
+db.COLLECTION_NAME.insert(document)
+db.COLLECTION_NAME.find()
+db.mycol.find({key1:value1, key2:value2}).pretty()
+db.mycol.find(
+   {
+      $or: [
+	     {key1: value1}, {key2:value2}
+      ]
+   }
+).pretty()
+db.COLLECTION_NAME.update(SELECTIOIN_CRITERIA, UPDATED_DATA)
+db.mycol.update({'title':'MongoDB Overview'},{$set:{'title':'New MongoDB Tutorial'}},{multi:true})
+db.COLLECTION_NAME.save({_id:ObjectId(),NEW_DATA}) // replace tuple
+db.COLLECTION_NAME.remove(DELLETION_CRITTERIA)
+db.COLLECTION_NAME.remove(DELETION_CRITERIA,1) remove one tuple
+db.mycol.remove() remove all tuples
+db.mycol.find({},{"title":1,_id:0}) 1: show 0: hide
+*/
 
 // handle post request
 app.post('/login',function (req, res) {
@@ -35,17 +67,16 @@ app.post('/login',function (req, res) {
 	/* check database if username and password match */
 	// Use connect method to connect to the Server
 	mongoClient.connect(mongourl, function (err, db) {
-	  if (err) {
-	    console.log('Unable to connect to the mongoDB server. Error:', err);
-	  } else {
-	    //HURRAY!! We are connected. :)
-	    console.log('Connection established to', mongourl);
+		assert.equal(null, err);
+		/*
+		insertDocument(db, function() {
+		    db.close();
+		});
+		*/
+		findUsers(db, function() {
+		    db.close();
+		});
 
-	    // do some work here with the database.
-
-	    //Close connection
-	    db.close();
-	  }
 	});
 
 	// send response to the client
@@ -61,3 +92,32 @@ var server = app.listen(3000, function () {
 
   	console.log('Example app listening at http://%s:%s', host, port);
 });
+
+var insertDocument = function(db, callback) {
+   db.collection('users').insertOne( {
+
+				"first" : "zhi",
+				"last" : "hu",
+				"username" : "jkljlk",
+				"passwod" : "pwd",
+				"email" : "test@email.com",
+				"credibility" : "8"
+
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    
+  });
+};
+
+var findUsers = function(db, callback) {
+   var cursor =db.collection('users').find( );
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+         console.dir(doc);
+      } else {
+         //callback();
+      }
+   });
+};

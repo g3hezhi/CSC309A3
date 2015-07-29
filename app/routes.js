3f759a18821b1931aeaa486a95db152b6235935d
Victor_Gong <victorgong16@gmail.com>
2015-07-29 19:06:28 -0400

    Added an About page, will redesign as it looks very elementary at the moment.
    Added an account_edit page and a userinfo_edit page. Need to workout "Unable to POST error".

--- app/routes.js
+++ app/routes.js
@@ -20,10 +20,10 @@ module.exports = function(app, passport) {
 	 app.get('/contact', function(req, res) {
         res.render('contact.ejs'); // load the contact.ejs file
     });
-	app.get('/posting', function(req, res) {
-        res.render('posting.ejs'); // load the posting.ejs file
+	app.get('/about', function(req, res) {
+        res.render('about.ejs'); // load the about.ejs file
     });
-
+	
     //direct express to locate cssfiles 
     app.use(express.static(__dirname + '/public'));
     // =====================================
@@ -69,6 +69,53 @@ module.exports = function(app, passport) {
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+	
+	// =====================================
+    // EDIT ACCOUNT =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/account_edit', isLoggedIn, function(req, res) {
+        res.render('account_edit.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+	
+	// app.post('/account_edit', do all our passport stuff here);    
+    app.post('/account_edit', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/account_edit', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+	
+	// =====================================
+    // EDIT USER INFORMATION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/userinfo_edit', isLoggedIn, function(req, res) {
+        res.render('userinfo_edit.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+	
+	// app.post('/userinfo_edit', do all our passport stuff here);    
+    app.post('/userinfo_edit', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/userinfo_edit', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+	
+	// =====================================
+    // POSTING =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/posting', isLoggedIn, function(req, res) {
+        res.render('posting.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });

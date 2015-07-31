f54ac290b9e5b5898f4ed1c2a1b83a1d391aaef2
suman5214 <suman5214@hotmail.com>
2015-07-31 19:29:57 -0400

    heroku deply

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/A3' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://a3admin:123456@ds043388.mongolab.com:43388/heroku_s64flrv2' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };

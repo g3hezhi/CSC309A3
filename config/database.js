1784fff7c9cc00257c1bdd38a5c3f676f89d762d
suman5214 <suman5214@hotmail.com>
2015-07-31 21:51:19 -0400

    fixing heroku

--- config/database.js
+++ config/database.js
@@ -3,7 +3,5 @@ module.exports = {
 
     'url' : 'mongodb://a3admin:123456@ds043388.mongolab.com:43388/heroku_s64flrv2' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
-    //'url' : 'mongodb://a3admin:123456@ds043388.mongolab.com:43388/heroku_s64flrv2' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
 
 };

1d6f7c387dc287a7276e2bd3bf8d099a6c8540e8
suman5214 <suman5214@hotmail.com>
2015-08-03 14:07:17 -0400

    fix some admin functions

--- app/routes.js
+++ app/routes.js
@@ -426,11 +426,20 @@ module.exports = function(app, passport) {
         res.render('admin_login', { message: req.flash('loginMessage'), csrfToken: req.csrfToken() }); 
     });
     // process the login form
-    app.post('/admin_login', csrfProtection, passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+    app.post('/admin_login', csrfProtection, function(req,res){
+		req.sanitize('email').escape();
+		var email=req.body.email;
+		if(email!='admin@mail.com'){
+			res.render('admin_login', { message: 'Please use a Admin Account', csrfToken: req.csrfToken() }); 
+		}
+		else{
+			next();
+			}	
+		},passport.authenticate('local-login', {
+        successRedirect : '/admin', // redirect to the secure profile section
         failureRedirect : '/admin_login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
+        failureFlash : true // allow flash message
+		}));
 	
 	app.get('/admin', function(req, res) {
         res.render('admin.ejs'); // load the contact.ejs file
@@ -475,7 +484,7 @@ module.exports = function(app, passport) {
 			User.findOne({email:pid}, function(err, user) {
 				//res.render('modify_password.ejs',{user :user});
 
-				//user.password = userPass;
+				user.password = user.generateHash(userPass);
 				user.username = userName;
 				user.first = firstName;
 				user.last = lastName;
